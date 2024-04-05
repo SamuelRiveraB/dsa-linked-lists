96185b25d47ff96fcb2c692aec6ae9d99730b6d2
Samuel <samuelrivba@gmail.com>
2024-04-05 16:06:06 -0500

    Add pointer playground

--- linked.js
+++ linked.js
@@ -6,3 +6,9 @@ const basket = ["apples", "grapes", "pears"];
 // 8947 --> grapes
 //           8792 --> pears
 //                     372 --> null
+
+let obj1 = { a: true };
+let obj2 = obj1;
+obj1.a = "booya";
+delete obj1;
+console.log(obj1, obj2);
