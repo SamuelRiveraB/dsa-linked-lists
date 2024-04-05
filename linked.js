7d5eee594861182c10472689e797297a7b0e41a8
Samuel <samuelrivba@gmail.com>
2024-04-05 16:05:48 -0500

    Add file with linked list schema

---
+++ linked.js
@@ -0,0 +1,8 @@
+const basket = ["apples", "grapes", "pears"];
+
+// linked list: apples --> grapes --> pears
+
+// apples
+// 8947 --> grapes
+//           8792 --> pears
+//                     372 --> null
